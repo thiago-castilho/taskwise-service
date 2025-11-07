980ac718b8621db33af59077f5e1a743d4623ecb
Thiago Castilho <tcosta1996@hotmail.com>
2025-11-07 02:57:26 -0300

    feat: add tests for invalid login credentials handling

--- test/login.test.js
+++ test/login.test.js
@@ -17,5 +17,35 @@ describe('Login', () => {
                 expect(response.body.user.id).to.not.be.null;
                 expect(response.body.user.name).to.equal('Admin');
         });
+
+        it('Deve retornar 401 com mensagem de erro ao usar senha inválida', async() => {
+            const bodyLogin = { ...postLogin };
+            bodyLogin.password = "123";
+
+            const response = await request(process.env.BASE_URL)
+                .post('/auth/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin);
+
+                expect(response.status).to.equal(401);
+                expect(response.body[0].code).to.be.equal('INVALID_CREDENTIALS');
+                expect(response.body[0].field).to.be.equal('password');
+                expect(response.body[0].message).to.be.equal('Senha inválida');
+        });
+
+        it('Deve retornar 401 com mensagem de erro ao usar email inválido', async() => {
+            const bodyLogin = { ...postLogin };
+            bodyLogin.email = "email@invalido.com";
+
+            const response = await request(process.env.BASE_URL)
+                .post('/auth/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin);
+
+                expect(response.status).to.equal(401);
+                expect(response.body[0].code).to.be.equal('INVALID_CREDENTIALS');
+                expect(response.body[0].field).to.be.equal('email');
+                expect(response.body[0].message).to.be.equal('Usuário não encontrado');
+        });
     });
 });
